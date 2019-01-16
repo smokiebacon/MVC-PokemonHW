81662e2ddb7538d4eee85cb5949fde59893ddd1c
Abraham Hsu <smokiebacon@gmail.com>
2019-01-16 12:57:36 -0800

    added delete functionality

--- js/server.js
+++ js/server.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
 app.use('/assets', express.static('assets'))
 
 const Pokemon = require('../models/pokemon');
@@ -14,12 +17,6 @@ app.get('/pokemon', (req, res) => {
     });
 })
 
-//POST route
-app.post('/pokemon', (req, res) => {
-    Pokemon.push(req.body);
-    res.redirect('/pokemon');
-})
-
 
 //new route
 app.get('/pokemon/new', (req, res) => {
@@ -28,20 +25,38 @@ app.get('/pokemon/new', (req, res) => {
 
 
 
+//EDIT ROUTE
+app.get('/pokemon/:id/edit', (req,res) => {
+    res.render('edit.ejs', {
+        pokemon : Pokemon[req.params.id],
+        index: req.params.id
+    });
+});
+
+//POST route
+app.post('/pokemon', (req, res) => {
+    Pokemon.push(req.body);
+    res.redirect('/pokemon');
+})
+
+//PUT or UPDATE route
+app.put('/pokemon/:id', (req, res) => {
+    Pokemon[req.params.id] = req.body;
+    res.redirect('/pokemon');
+})
 
 //show route
 app.get('/pokemon/:id', (req,res) => {
-    
     res.render('show.ejs', {
         pokemon : Pokemon[req.params.id]
     })
 })
 
-
 //delete pokemon route
-
-//update pokemon route
-
+app.delete('/pokemon/:id', (req, res) => {
+    Pokemon.splice(req.params.id, 1);
+    res.redirect('/pokemon')
+})
 
 
 app.listen(3000, () => {
